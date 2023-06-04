d3fe919ab4b74e38987d5321cedf558d8262daf2
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-06-04 20:23:51 +0200

    now rly

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Causes } from "./Causes";
+import { Jezsuita } from "./Jezsuita";
+import { Kalvin } from "./Kalvin";
+import { Luther } from "./Luther";
+import { Trident } from "./Trident";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <p>Reformácio</p>
       <Causes />
+      <Luther />
+      <Kalvin />
+      <Jezsuita />
+      <Trident />
     </main>
   );
 }
